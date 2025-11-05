84a7cb06c068a2919f53a49f6f7ef5f535aac657
KartikK2004 <kartikkadam175@gmail.com>
2025-11-05 21:35:35 +0530

    first commit

---
+++ O1-basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";//treat all Js code as a newer version
+
+console.log(5
+    +
+    5); //code readability should be high
+
+
+console.log("Kartik");
+
+//DataType of javascript
+
+let name = "kartik"
+let age = 18;
+let isLoggedIn = false
+
+
+//Number => 2 to Power of 53
+//Bigint
+//String =>""
+//Boolean =>true/false
+//null => Standalone value ,Type of Object
+console.log(typeof null);//Object
+//Undefined =>Not assigned value ,Type of Undefined
+console.log(typeof undefined);//Undefined
+
+//symbol => unique
+
+
+//Object
+
+
+
