a4881f0fc1f91ee33398beba9a08a767a9ef558f
KartikK2004 <kartikkadam175@gmail.com>
2025-11-06 19:14:31 +0530

    Conversion

---
+++ O1-basics/03_ConversionOperation.js
@@ -0,0 +1,36 @@
+let Score = "45abc"
+
+//console.log(typeof Score);
+//console.log((Score));
+
+let valueINNumber = Number(Score)
+//console.log(typeof valueINNumber);
+//console.log(valueINNumber);
+
+//"33"=>33
+//"33abc"=>NaN//
+//true=>1; false =>0
+
+// let UserLogIn = 1;
+// let BolleanIsLogin = Boolean(UserLogIn)
+// console.log(BolleanIsLogin);
+
+
+let UserLogout = 0
+let BolleanIsLogout = Boolean(UserLogout)
+//console.log(BolleanIsLogout);
+
+
+//1 =>True
+//0 =>False
+
+//"" =>False
+//"Kartik" =>True
+
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+
+
+
