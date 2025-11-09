4aa970039e006bf3434aa8c87898f31b21dc1b25
KartikK2004 <kartikkadam175@gmail.com>
2025-11-09 12:57:30 +0530

    Dates

---
+++ O1-basics/07_Dates.js
@@ -0,0 +1,48 @@
+//Dates
+
+let MyDate = new Date()
+// console.log(MyDate);
+// console.log(MyDate.toString());
+// console.log(MyDate.toJSON());
+// console.log(MyDate.toLocaleDateString());
+// console.log(MyDate.toLocaleString());
+// console.log(typeof MyDate);
+
+// let MyCreatedDate = new Date(2004, 0, 15)
+// console.log(MyCreatedDate.toLocaleString());
+// console.log(MyCreatedDate.toString());
+// console.log(MyCreatedDate.toDateString());
+
+// let MyCreatedDate = new Date("2003-01-15")// Y/M/D
+let MyCreatedDate = new Date("01-15-2004") //M/D/Y
+// console.log(MyCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay() + 1);
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+
+})
+//This well help you to modify the value as per your requirement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
