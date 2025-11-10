5b1e2f1f814f984eb93880c4c07208e651e79cc2
KartikK2004 <kartikkadam175@gmail.com>
2025-11-10 21:28:19 +0530

    Array

---
+++ 02-Basics/Array.js
@@ -0,0 +1,76 @@
+//array is the datastructure its used to stores the collection of elements
+
+//This elements can de various datatypes like 
+
+["Number", "String", "Boolean", "object", "function"]
+
+
+const myarr = [0, 1, 2, 3, 4, 5, true, "kartik"]
+
+const myheros = ["shaktiman", "Superman"]
+
+const myArr = new Array(1, 2, 3, 4)
+
+console.log(myarr[3]);
+
+//ARRAY METHODS****************
+
+myarr.push(6)//Its add the element to the end of an array
+myarr.push(7)//Its add the element to the end of an array
+myarr.pop()//Its remove the last element
+console.log(myarr);
+
+const Newarray = [1, 2, 3, 4, 5, 6, 7, 8]
+Newarray.unshift(0)//Its add the element to the starting point
+Newarray.shift()//Its remove the Starting element
+
+console.log(Newarray.includes(2));// Includes Method are used to check element is present or not
+
+const Joinarray = Newarray.join()//Join method is used to Joins all elements of an array into string
+// console.log(typeof Joinarray);
+// console.log(Newarray);
+
+//Slice and splice
+
+console.log("A", Newarray);
+
+// const myn1 = Newarray.slice(1, 4)
+console.log(myn1);
+console.log("B", Newarray);
+
+// const myn2 = Newarray.splice(1, 4)
+console.log("c", Newarray);
+console.log(myn2);
+
+let fruit = ["Apple", "Mango", "Orange", "kiwi", "Banana", "Lemon"]
+console.log(fruit.slice(2, 4));//It extract the section of an array And returns new array
+console.log(fruit.splice(0, 3));//The Splice method adds or removes array elements
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ O1-basics/04_Comparision.js
@@ -0,0 +1,34 @@
+//*************Comparision******************/
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //True 
+// //Comparisions convert null to number, treating it as 0 That's why its true 
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+//===
+//Its check the Datatype Also
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
