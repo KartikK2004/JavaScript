563a0df34fb97b03a7a1141d45cd3f41b19cff1e
KartikK2004 <kartikkadam175@gmail.com>
2025-11-10 21:53:56 +0530

    Files add

---
+++ O1-basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "kartikkadam"
+var accountPassword = "12345"
+// accountCity = "panvel"
+let accountState;
+
+
+accountEmail = "kadam@gmail.com"
+accountPassword = "313131"
+
+
+/*
+
+Prefer not to use var because of issue in block scope and functional scope
+
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountState,])
